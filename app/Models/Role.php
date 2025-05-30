33f8fcddb53256719439623d87b4b6a8159b5e68
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-30 22:56:08 +0700

    fix model role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Role extends Model
 {
     use HasFactory, Notifiable, SoftDeletes, AuditedBySoftDelete;
-    protected $table = 'status';
+    protected $table = 'role';
     protected $guarded = ['id'];
 
     public function statu()
