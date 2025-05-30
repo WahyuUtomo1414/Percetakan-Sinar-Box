8f20c618f847698dba7a496faadc823a158a82dd
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-30 23:08:09 +0700

    buat table payment method

--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -8,10 +8,10 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Role extends Model
+class PaymentMethod extends Model
 {
     use HasFactory, Notifiable, SoftDeletes, AuditedBySoftDelete;
-    protected $table = 'role';
+    protected $table = 'payment_method';
     protected $guarded = ['id'];
 
     public function status()
