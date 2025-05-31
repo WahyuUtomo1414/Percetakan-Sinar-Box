c98489cf97432b9488fbceb028bfc62d5245a685
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-31 23:23:09 +0700

    buat payment status sedder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductStatusSeeder::class,
             OrdersStatusSeeder::class,
+            PaymentStatusSeeder::class,
         ]);
     }
 }
