4fa99bee97912cddb6933b64395c4f8572a8bbed
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-31 23:38:29 +0700

    hapus fill desc di shiping seeder

--- database/seeders/ShipingSeeder.php
+++ database/seeders/ShipingSeeder.php
@@ -16,37 +16,37 @@ class ShipingSeeder extends Seeder
         $shipping = [
             [
                 'name' => 'JNE',
-                'desc' => 'Delivery within 5-7 business days',
+                'desc' => '',
                 'price' => 12000,
                 'status_id' => 1,
             ],
             [
                 'name' => 'JNT',
-                'desc' => 'Delivery within 3-5 business days',
+                'desc' => '',
                 'price' => 15000,
                 'status_id' => 1,
             ],
             [
                 'name' => 'POS Indonesia',
-                'desc' => 'Delivery within 7-10 business days',
+                'desc' => '',
                 'price' => 10000,
                 'status_id' => 1,
             ],
             [
                 'name' => 'Gojek',
-                'desc' => 'Delivery within 1-2 business days',
+                'desc' => '',
                 'price' => 20000,
                 'status_id' => 1,
             ],
             [
                 'name' => 'Grab',
-                'desc' => 'Delivery within 1-2 business days',
+                'desc' => '',
                 'price' => 20000,
                 'status_id' => 1,
             ],
             [
                 'name' => 'SiCepat',
-                'desc' => 'Delivery within 1-2 business days',
+                'desc' => '',
                 'price' => 15000,
                 'status_id' => 1,
             ],
